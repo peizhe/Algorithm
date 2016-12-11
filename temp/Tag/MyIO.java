de6bb83d6158e6408d894084a8e4acceb260b08b
cold2wind <923762584@qq.com>
2016-12-11 23:42:03 +0800

    algorithm

---
+++ temp/Tag/MyIO.java
@@ -0,0 +1,63 @@
+package cn.bupt.sse309.wisdomAgriculture.backgroundService.Tag;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyIO {
+	public static String readFromIn(InputStream in){
+		/*BufferedReader reader = new BufferedReader(new InputStreamReader(in));   
+        StringBuilder sb = new StringBuilder();   
+        String line = null;   
+        try {   
+            while ((line = reader.readLine()) != null) {   
+                sb.append(line + "/n");   
+            }   
+        } catch (IOException e) {   
+            e.printStackTrace();   
+        } finally {   
+            try {   
+                in.close();   
+                reader.close();
+            } catch (IOException e) {   
+                e.printStackTrace();   
+            }   
+        }
+		return line;   
+
+	    */
+		try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] temp = new byte[1024];
+            for (;;) {
+                int size = in.read(temp);
+                if (size != -1) {
+                    out.write(temp, 0, size);
+                } else {
+                    break;
+                }
+            }
+            String result = new String(out.toByteArray());
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+	   
+	}
+
+	public static boolean writetoFile(File file,String content){
+		FileWriter filewrite;
+		try {
+			filewrite = new FileWriter(file);
+			filewrite.write(content);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
---
+++ temp/Tag/json/ImageKeys.java
@@ -0,0 +1,16 @@
+package cn.bupt.sse309.wisdomAgriculture.backgroundService.Tag.json;
+
+import java.util.List;
+
+public class ImageKeys {
+	private List<ImageKey> tags;
+
+	public List<ImageKey> getKeyList() {
+		return tags;
+	}
+
+	public void setKeyList(List<ImageKey> tags) {
+		this.tags = tags;
+	}
+	
+}
